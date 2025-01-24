ed4c1defc8b88dbd16e611f6a750757ea57ead86
김의중 <ejk5148@gmail.com>
2025-01-24 09:39:50 +0900

    init: 스프링부트 초기세팅

---
+++ backend/src/test/java/com/ssafy12/moinsoop/skinfit/SkinfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy12.moinsoop.skinfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
