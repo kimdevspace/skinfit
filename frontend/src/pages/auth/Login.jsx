43aa10652f38b0026f44063584604beb4547dc89
hannah <hanna0115@naver.com>
2025-02-02 02:09:08 +0900

    Design: 로그인 페이지 수정 완료
    재사용 가능한 로그인 폼 컴포넌트화 및 로그인 페이지 수정 완료

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import "./Login.scss";
-import invisibleEye from "../../assets/icons/login/invisibleEye.png";
-import visibleEye from "../../assets/icons/login/visibleEye.png";
 import Logo from "../../components/common/Logo.jsx";
+import AuthBox from "../../components/auth/AuthBox.jsx";
 import Button from "../../components/common/Button.jsx";
 import kakaoLoginBtn from "../../assets/images/kakao_login_medium_wide.png";
 
@@ -18,75 +17,34 @@ function Login() {
     console.log("로그인:", { email, password });
   };
 
-  const togglePasswordVisibility = () => {
-    setShowPassword((prev) => !prev);
-  };
-
-  const handleSignUpClick = () => {
-    setShowSignUp(true);
-  };
+  // const handleSignUpClick = () => {
+  //   setShowSignUp(true);
+  // };
 
   return (
     <div className="login-container">
       {/* 상단 로고 */}
       <Logo />
 
-      {/* 가운데 정렬된 래퍼 */}
-      <div className="box-wrapper">
-        {/* 배경 박스 */}
-        <div className="login-box-bg"></div>
-
-        {/* 로그인 박스 */}
-        <div className={`login-box-front ${showSignUp ? "move-box" : ""}`}>
-          <h2 className="login-box-title">LOGIN</h2>
-
-          {/* SIGN UP 버튼 */}
-          <button className="signup-circle" onClick={handleSignUpClick}>
+      {/* SIGN UP 버튼 */}
+      {/* <button className="signup-circle" onClick={handleSignUpClick}>
             SIGN UP
-          </button>
+          </button> */}
 
-          {/* 폼 */}
-          <form onSubmit={handleLoginSubmit}>
-            <label htmlFor="email">이메일</label>
-            <input
-              id="email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+      {/* 로그인 폼 */}
+      <form onSubmit={handleLoginSubmit}>
+        <AuthBox />
+        <Button text="로그인" color="white" type="submit" />
+      </form>
 
-            <label htmlFor="password">비밀번호</label>
-            <div className="password-wrapper">
-              <input
-                id="password"
-                type={showPassword ? "text" : "password"}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <span className="toggle-eye" onClick={togglePasswordVisibility}>
-                {showPassword ? (
-                  <img src={invisibleEye} alt="invisibleEye" />
-                ) : (
-                  <img src={visibleEye} alt="visibleEye" />
-                )}
-              </span>
-            </div>
+      <p className="password-find">비밀번호 찾기 | 회원가입</p>
+      <hr className="divider" />
 
-            <Button text="로그인" color="white" type="submit" />
-          </form>
-
-          <p className="password-find">비밀번호 찾기</p>
-          <hr className="divider" />
-
-          <img
-            className="kakao-login-btn"
-            src={kakaoLoginBtn}
-            alt="kakao-login-btn"
-          />
-        </div>
-      </div>
+      <img
+        className="kakao-login-btn"
+        src={kakaoLoginBtn}
+        alt="kakao-login-btn"
+      />
     </div>
   );
 }
