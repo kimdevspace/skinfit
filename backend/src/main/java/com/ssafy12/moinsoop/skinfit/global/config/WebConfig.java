15a9545296e6101c8b5de7a3d5784c96d4b2b9b3
김의중 <ejk5148@gmail.com>
2025-02-03 11:14:01 +0900

    Feat: JavaMailSender Bean 등록
    JIRA: #S12P11B111-77

--- backend/src/main/java/com/ssafy12/moinsoop/skinfit/global/config/WebConfig.java
+++ backend/src/main/java/com/ssafy12/moinsoop/skinfit/global/config/WebConfig.java
@@ -2,6 +2,8 @@ package com.ssafy12.moinsoop.skinfit.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,5 +12,11 @@ public class WebConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        return mailSender;
     }
 }
