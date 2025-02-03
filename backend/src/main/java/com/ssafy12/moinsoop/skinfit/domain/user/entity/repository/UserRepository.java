a3fa6e63d664b3f43df3576a4999a9359b8307e7
김의중 <ejk5148@gmail.com>
2025-02-03 11:14:33 +0900

    Feat: UserRepository에 이메일 중복검사 추가
    JIRA: #S12P11B111-77

--- backend/src/main/java/com/ssafy12/moinsoop/skinfit/domain/user/entity/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy12/moinsoop/skinfit/domain/user/entity/repository/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUserEmail(String userEmail);
+    boolean existsByUserEmail(String userEmail);
 }
