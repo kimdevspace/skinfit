ad5062de3af54af0728295dbecd2a402db550fc1
hyejoony <jeonbok.joha@gmail.com>
2025-02-04 17:59:33 +0900

    Feat : 화장품명 검색 기능 추가
    api 요청(쿼리 파라미터)에 따른 화장품명 검색색 store구축

--- frontend/src/stores/Search.js
+++ frontend/src/stores/Search.js
@@ -4,39 +4,93 @@ import { create } from 'zustand';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
-//#region 모든 화장품 조회
+//#region 화장품명 검색
 // 액션
-const fetchAllCosmetics = async () => {
-  const response = await axios.get('/api/v1/all-cosmetics');
+const FetchCosmetics = async (params) => {
+  const response = await axios.get('/api/v1/search/cosmetic', {
+    params, 
+    // headers : {
+    //     Authorization : 
+    // }
+  });
+  
   if (response.data.status === 'success') {
     return response.data.data;
   }
-  throw new Error('Failed to fetch cosmetics');
+  throw new Error(response.data.message || 'Failed to fetch cosmetics');
 };
 
 //set 함수 : zustand store 업데이트 함수
 // create : 스토어 생성 
 const useSearchStore = create((set) => ({
-  setCosmetics: (cosmetics) => set({ cosmetics }),
-  setTotalCount: (totalCount) => set({ totalCount }),
-}));
+    query: '',
+    page: 1,
+    limit: 10,
+    filterByUserPreference: false,
+    category: null,
+    setQuery: (query) => set({ query }),
+    setPage: (page) => set({ page }),
+    setLimit: (limit) => set({ limit }),
+    setFilterByUserPreference: (filter) => set({ filterByUserPreference: filter }),
+    setCategory: (category) => set({ category }),
+  }));
+
 
 // React Query hook (캐싱된 데이터를 불러온다다)
-export const useAllCosmetics = () => {
-  const setCosmetics = useSearchStore((state) => state.setCosmetics);
-  const setTotalCount = useSearchStore((state) => state.setTotalCount);
+export const useSearchCosmetics = () => {
+  const setQuery = useSearchStore((state) => state.setQuery)
+  const setPage = useSearchStore((state) => state.setPage)
+  const setFilterByUserPreference = useSearchStore((state) => state.setFilterByUserPreference)
+  const setCategory = useSearchStore((state) => state.setCategory)
 
     //useQuery 는 서버로부터 데이터를 요청하여 받아오는 GET api 
     // const {data} = useQuery(쿼리 키, 쿼리 함수, 옵션);
     // 쿼리함수 => 비동기 익명 함수
   return useQuery({
-    queryKey : ['allCosmetics'],
-    queryFn: fetchAllCosmetics,
+    queryKey : ['searchCosmetics'],
+    queryFn: FetchCosmetics,
     // 성공시 콜백함수 
     onSuccess: (data) => {
-      setCosmetics(data.cosmetic);
-      setTotalCount(data.totalCount);
+      setQuery(data.query);
+      setPage(data.page);
     },
   });
 };
+//#endregion 
+
+// //#region 모든 화장품 조회
+// // 액션
+// const fetchAllCosmetics = async () => {
+//   const response = await axios.get('/api/v1/all-cosmetics');
+//   if (response.data.status === 'success') {
+//     return response.data.data;
+//   }
+//   throw new Error('Failed to fetch cosmetics');
+// };
+
+// //set 함수 : zustand store 업데이트 함수
+// // create : 스토어 생성 
+// const useSearchStore = create((set) => ({
+//   setCosmetics: (cosmetics) => set({ cosmetics }),
+//   setTotalCount: (totalCount) => set({ totalCount }),
+// }));
+
+// // React Query hook (캐싱된 데이터를 불러온다다)
+// export const useAllCosmetics = () => {
+//   const setCosmetics = useSearchStore((state) => state.setCosmetics);
+//   const setTotalCount = useSearchStore((state) => state.setTotalCount);
+
+//     //useQuery 는 서버로부터 데이터를 요청하여 받아오는 GET api 
+//     // const {data} = useQuery(쿼리 키, 쿼리 함수, 옵션);
+//     // 쿼리함수 => 비동기 익명 함수
+//   return useQuery({
+//     queryKey : ['allCosmetics'],
+//     queryFn: fetchAllCosmetics,
+//     // 성공시 콜백함수 
+//     onSuccess: (data) => {
+//       setCosmetics(data.cosmetic);
+//       setTotalCount(data.totalCount);
+//     },
+//   });
+// };
 //#endregion
