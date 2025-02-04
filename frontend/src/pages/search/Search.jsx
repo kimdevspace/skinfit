e12c810769dc44c39b8c6b204a8c88e70a531d13
Hye-joon Jeon <jeonbok.joha@gmail.com>
2025-02-04 21:17:54 +0900

    Chore : 코드 다듬기기
    화장품명 검색 로직 구현을 위해 코드 다듬기(기존 실시간 검색어 연동과 섞여있었음)

--- frontend/src/pages/search/Search.jsx
+++ frontend/src/pages/search/Search.jsx
@@ -2,32 +2,33 @@ import SearchBar from "../../components/search/SearchBar.jsx"
 import SearchItem from "../../components/search/SearchItem.jsx"
 import SearchResult from "../../components/search/SearchResult.jsx"
 import { useState, useEffect, useMemo } from "react"
-import useSearchStore, { useAllCosmetics } from '../stores/Search'; // 검색 store (pinia)
+import useSearchStore, { useSearchCosmetics} from '../stores/Search'; // 검색 store (pinia)
 
 function Search() {
 
-  //실시간 검색어 연동
+  //검색어 연동
   //setSearchWord : searchWord 변수를 바꿔주는 애 
   const [searchWord, setSearchWord] = useState('')
 
-  //모든 화장품 조회
-  const { isLoading, isError, error } = useAllCosmetics();
-  const cosmetics = useSearchStore(state => state.cosmetics);
- 
-  // 실시간 검색어-연관검색어 목록 
-  // useMemo : computed 
-  const filteredCosmetics = useMemo(() => {
-    // if (!cosmetics) return [];
-    return cosmetics.filter(item => 
-      item.cosmeticName.includes(searchWord) //제품명만 반환  
-    );
-  }, [cosmetics, searchWord]); 
+  //특정 화장품명 검색 
+  const { isLoading, isError, error } = useSearchCosmetics();
+
 
   // 로딩 상태와 에러 상태를 useEffect를 사용하여 로그로 출력
   useEffect(() => {
     if (isLoading) console.log('로딩 중');
     if (isError) console.log('에러', error?.message);
   }, [isLoading, isError, error]);
+
+
+  // // 실시간 검색어-연관검색어 목록 
+  // // useMemo : computed 
+  // const filteredCosmetics = useMemo(() => {
+  //   // if (!cosmetics) return [];
+  //   return cosmetics.filter(item => 
+  //     item.cosmeticName.includes(searchWord) //제품명만 반환  
+  //   );
+  // }, [cosmetics, searchWord]); 
 
   return (
     <div>
@@ -41,7 +42,7 @@ function Search() {
       {/* <SearchResult searchWord={searchWord}/>  */}
 
       {/* 실시간 검색어 포함된 화장품명 리스트 전달 */}
-      <SearchResult  cosmetics={filteredCosmetics}/> 
+      <SearchResult  cosmetics={}/> 
 
       {/* 검색버튼 사용자가 누를시 SearchResult 컴포넌트 제거하고 SearchItem 띄우기 */}
       
--- frontend/src/stores/Search.js
+++ frontend/src/stores/Search.js
@@ -1,40 +1,40 @@
 //react query 기반(key값 조회, 캐싱싱)
 
-import { create } from 'zustand';
-import { useQuery } from 'react-query';
-import axios from 'axios';
+import { create } from "zustand"
+import axios from "axios"
+import { useQuery } from "@tanstack/react-query"
 
 //#region 화장품명 검색
 // 액션
 const FetchCosmetics = async (params) => {
-  const response = await axios.get('/api/v1/search/cosmetic', {
-    params, 
+  const response = await axios.get("/api/v1/search/cosmetic", {
+    params,
     // headers : {
-    //     Authorization : 
+    //     Authorization :
     // }
-  });
-  
-  if (response.data.status === 'success') {
-    return response.data.data;
+  })
+
+  if (response.data.status === "success") {
+    return response.data.data
   }
-  throw new Error(response.data.message || 'Failed to fetch cosmetics');
-};
+  throw new Error(response.data.message || "Failed to fetch cosmetics")
+}
 
 //set 함수 : zustand store 업데이트 함수
-// create : 스토어 생성 
+// create : 스토어 생성
 const useSearchStore = create((set) => ({
-    query: '',
-    page: 1,
-    limit: 10,
-    filterByUserPreference: false,
-    category: null,
-    setQuery: (query) => set({ query }),
-    setPage: (page) => set({ page }),
-    setLimit: (limit) => set({ limit }),
-    setFilterByUserPreference: (filter) => set({ filterByUserPreference: filter }),
-    setCategory: (category) => set({ category }),
-  }));
-
+  // 디폴트값 세팅
+  query: "",
+  page: 1,
+  limit: 10,
+  filterByUserPreference: false,
+  category: null,
+  setQuery: (query) => set({ query }),
+  setPage: (page) => set({ page }),
+  setLimit: (limit) => set({ limit }),
+  setFilterByUserPreference: (filter) => set({ filterByUserPreference: filter }),
+  setCategory: (category) => set({ category }),
+}))
 
 // React Query hook (캐싱된 데이터를 불러온다다)
 export const useSearchCosmetics = () => {
@@ -43,20 +43,22 @@ export const useSearchCosmetics = () => {
   const setFilterByUserPreference = useSearchStore((state) => state.setFilterByUserPreference)
   const setCategory = useSearchStore((state) => state.setCategory)
 
-    //useQuery 는 서버로부터 데이터를 요청하여 받아오는 GET api 
-    // const {data} = useQuery(쿼리 키, 쿼리 함수, 옵션);
-    // 쿼리함수 => 비동기 익명 함수
+  //useQuery 는 서버로부터 데이터를 요청하여 받아오는 GET api
+  // const {data} = useQuery(쿼리 키, 쿼리 함수, 옵션);
+  // 쿼리함수 => 비동기 익명 함수
   return useQuery({
-    queryKey : ['searchCosmetics'],
+    queryKey: ["searchCosmetics"],
     queryFn: FetchCosmetics,
-    // 성공시 콜백함수 
+    // 성공시 콜백함수
     onSuccess: (data) => {
-      setQuery(data.query);
-      setPage(data.page);
+      setQuery(data.query)
+      setPage(data.page)
+      setFilterByUserPreference(data.filterByUserPreference)
+      setCategory(data.category)
     },
-  });
-};
-//#endregion 
+  })
+}
+//#endregion
 
 // //#region 모든 화장품 조회
 // // 액션
@@ -69,7 +71,7 @@ export const useSearchCosmetics = () => {
 // };
 
 // //set 함수 : zustand store 업데이트 함수
-// // create : 스토어 생성 
+// // create : 스토어 생성
 // const useSearchStore = create((set) => ({
 //   setCosmetics: (cosmetics) => set({ cosmetics }),
 //   setTotalCount: (totalCount) => set({ totalCount }),
@@ -80,13 +82,13 @@ export const useSearchCosmetics = () => {
 //   const setCosmetics = useSearchStore((state) => state.setCosmetics);
 //   const setTotalCount = useSearchStore((state) => state.setTotalCount);
 
-//     //useQuery 는 서버로부터 데이터를 요청하여 받아오는 GET api 
+//     //useQuery 는 서버로부터 데이터를 요청하여 받아오는 GET api
 //     // const {data} = useQuery(쿼리 키, 쿼리 함수, 옵션);
 //     // 쿼리함수 => 비동기 익명 함수
 //   return useQuery({
 //     queryKey : ['allCosmetics'],
 //     queryFn: fetchAllCosmetics,
-//     // 성공시 콜백함수 
+//     // 성공시 콜백함수
 //     onSuccess: (data) => {
 //       setCosmetics(data.cosmetic);
 //       setTotalCount(data.totalCount);
