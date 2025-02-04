c50e2381d7c47e32343993a1f53a0efdaa0a67ec
Hye-joon Jeon <jeonbok.joha@gmail.com>
2025-02-05 01:42:10 +0900

    Feat : 검색 결과 컴포넌트 완성
    searchitem과 searchresult 컴포넌트와 헷갈렸었던 점을 제대로 잡고,  UI 및 로직 연결 완료
    
    JIRA : #S12P11B111-55

--- frontend/src/components/search/SearchResult.jsx
+++ frontend/src/components/search/SearchResult.jsx
@@ -1,49 +1,24 @@
 //SearchResult : 검색 결과(검색 결과 없어요, 검색 결과 리스트(searchItem) -> if뮨 처리)
 import "./SearchResult.scss"
-import { useEffect, useState } from "react";
+import SearchItem from "../../components/search/SearchItem.jsx"
 
-function SearchResult({  }) {
+// 서치 아이템 컴포넌트트
+// 아래 코드 정리
 
-  //검색 액션 여부 -> 조건부 랜더링링
-  const [hasResults, setHasResults] = useState(false);
-
-  // for문으로 실시간 연관검색어 목록 반환
-  useEffect(() => {
-    const checkResults = async () => {
-      const results = await fetchSearchResults();
-      setHasResults(results.length > 0);
-    };
-
-    checkResults();
-  }, []);
-
-  // 검색어 입력 글자수 제한
-  // 임의로 25글자 이상일 때 .. 처리
-  const truncateText = (text, maxLength) => {
-    if (text.length > maxLength) {
-      return text.slice(0, maxLength) + ".."
-    }
-    return text
-  }
-
+function SearchResult({ cosmetics }) {
   return (
     <>
-    {hasResults? ( 
-    <div className='search-wrapper2'>
-      <div className="search-result">
-        {/* 검색결과받기 */}
-        <p>{truncateText( , 25)}</p>
-      </div>
-      <hr />
-    </div>
-    ) : (
-    <div className="wrong-search">
-      <h2>검색 결과가 없어요</h2>
-      <p>철자를 확인하거나 다른 키워드로 검색해주세요</p>
-      <p className="ptag-2">등록되지 않은 화장품이라면?</p>
-      <button className="register-btn">사진 찍어 화장품 등록하기</button>
-    </div>
-    )}
+      {cosmetics ? (
+        // 반복문으로 검색 목록 풀어서, searchitem에 하나씩 전달
+        cosmetics.map((cosmetic) => <SearchItem key={cosmetic.id} cosmetic={cosmetic} />)
+      ) : (
+        <div className="wrong-search">
+          <h2>검색 결과가 없어요</h2>
+          <p>철자를 확인하거나 다른 키워드로 검색해주세요</p>
+          <p className="ptag-2">등록되지 않은 화장품이라면?</p>
+          <button className="register-btn">사진 찍어 화장품 등록하기</button>
+        </div>
+      )}
     </>
   )
 }
