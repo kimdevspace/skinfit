93ca4c08d63d960d612e911a22901f48e137e688
hyejoony <jeonbok.joha@gmail.com>
2025-02-04 17:12:02 +0900

    Fix : 실시간 검색어 연동
    검색바 실시간 검색어 검색목록과 연동 초안 (참고로 담당영역이 아니었어서 다음에 활용 필요)

--- frontend/src/components/search/SearchResult.jsx
+++ frontend/src/components/search/SearchResult.jsx
@@ -2,22 +2,20 @@
 import "./SearchResult.scss"
 import { useEffect, useState } from "react";
 
-function SearchResult({ searchWord }) {
+function SearchResult({  }) {
 
   //검색 액션 여부 -> 조건부 랜더링링
   const [hasResults, setHasResults] = useState(false);
 
-  //실시간 연관검색어 
+  // for문으로 실시간 연관검색어 목록 반환
   useEffect(() => {
     const checkResults = async () => {
-      // 여기에 실제 검색 결과를 확인하는 로직을 구현합니다.
-      // 예시: API 호출 또는 데이터 처리
-      const results = await fetchSearchResults(searchWord);
+      const results = await fetchSearchResults();
       setHasResults(results.length > 0);
     };
 
     checkResults();
-  }, [searchWord]);
+  }, []);
 
   // 검색어 입력 글자수 제한
   // 임의로 25글자 이상일 때 .. 처리
@@ -34,7 +32,7 @@ function SearchResult({ searchWord }) {
     <div className='search-wrapper2'>
       <div className="search-result">
         {/* 검색결과받기 */}
-        <p>{truncateText(searchWord , 25)}</p>
+        <p>{truncateText( , 25)}</p>
       </div>
       <hr />
     </div>
--- frontend/src/pages/search/Search.jsx
+++ frontend/src/pages/search/Search.jsx
@@ -1,15 +1,33 @@
 import SearchBar from "../../components/search/SearchBar.jsx"
 import SearchItem from "../../components/search/SearchItem.jsx"
 import SearchResult from "../../components/search/SearchResult.jsx"
-import { useState } from "react"
-
+import { useState, useEffect, useMemo } from "react"
+import useSearchStore, { useAllCosmetics } from '../stores/Search'; // 검색 store (pinia)
 
 function Search() {
 
-  //실시간 검색어어 연동
+  //실시간 검색어 연동
   //setSearchWord : searchWord 변수를 바꿔주는 애 
   const [searchWord, setSearchWord] = useState('')
 
+  //모든 화장품 조회
+  const { isLoading, isError, error } = useAllCosmetics();
+  const cosmetics = useSearchStore(state => state.cosmetics);
+ 
+  // 실시간 검색어-연관검색어 목록 
+  // useMemo : computed 
+  const filteredCosmetics = useMemo(() => {
+    // if (!cosmetics) return [];
+    return cosmetics.filter(item => 
+      item.cosmeticName.includes(searchWord) //제품명만 반환  
+    );
+  }, [cosmetics, searchWord]); 
+
+  // 로딩 상태와 에러 상태를 useEffect를 사용하여 로그로 출력
+  useEffect(() => {
+    if (isLoading) console.log('로딩 중');
+    if (isError) console.log('에러', error?.message);
+  }, [isLoading, isError, error]);
 
   return (
     <div>
@@ -19,7 +37,13 @@ function Search() {
       <SearchItem showBadge={true}  />
       <SearchItem showBadge={false} />
       
-      <SearchResult searchWord={searchWord}/> 
+      {/* 연습용 */}
+      {/* <SearchResult searchWord={searchWord}/>  */}
+
+      {/* 실시간 검색어 포함된 화장품명 리스트 전달 */}
+      <SearchResult  cosmetics={filteredCosmetics}/> 
+
+      {/* 검색버튼 사용자가 누를시 SearchResult 컴포넌트 제거하고 SearchItem 띄우기 */}
       
     </div>
   )
