3c544d0ed6dc0e4eb3db42aa1bfa881c11033d51
Hye-joon Jeon <jeonbok.joha@gmail.com>
2025-02-05 01:48:55 +0900

    Fix : 컴포넌트 서로 잘못 배치한거 수정 및 데이터 조회 로직 해결
    searchItem과 searchResult 잘못 배치 수정 후 로직 연결
    검색 결과 제품명 너무 길 경우 임의로 15글자 이상 '..'으로 대체
    
    JIRA : #S12P11B111-55

--- frontend/src/components/search/SearchItem.jsx
+++ frontend/src/components/search/SearchItem.jsx
@@ -1,31 +1,36 @@
-//searchItem.jsx : 검색결과 하나(뱃지, 버튼 Props로 바꾸기)
 // 검색완료 컴포넌트
+import "./SearchItem.scss"
 
-import "./SearchItem.scss";
+// 제품명 임의로 15글자 이상일 때 .. 처리
+const truncateText = (text, maxLength) => {
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + ".."
+  }
+  return text
+}
 
-
-// props로 뱃지 표시 여부 받기
-// 뱃지 or 추가 버튼
-function SearchItem({ showBadge = true }) {
+function SearchItem({ cosmetic }) {
   return (
     <div className="margin-setting">
-    <div className="search-wrapper">
-      <div className="search-name">
-        <img src="" alt="" />
-        <p className="brand-name">달바</p>
-        <p>달바 미스트 블랙 트러플 100ML</p>
-      </div>
-      <div className="badge-or-btn">
+      <div className="search-wrapper">
+        <div>
+          <img className="cosmetic-image" src={cosmetic.imageURL} alt="cosmetic_product_image" />
+          <p className="brand-name">{cosmetic.brandName}</p>
+          <p>{truncateText(cosmetic.cosmeticName, 15)}</p>
+        </div>
+
+        {/* 뱃지 or 추가버튼  */}
+        {/* <div className="badge-or-btn">
         {showBadge ? (
           <div className="badge isSafe">안전</div>
         ) : (
           <button className="add-btn">추가</button>
         )}
-      </div>
+      </div> */}
       </div>
       <hr />
     </div>
-  );
+  )
 }
 
-export default SearchItem;
+export default SearchItem
