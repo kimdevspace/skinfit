6ba24b816970084d16da9299bae8277f6a561d66
Hye-joon Jeon <jeonbok.joha@gmail.com>
2025-02-05 01:36:46 +0900

    Feat : tanstack query 사용을 위해 Provider 설정
    main.jsx에 연결

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './assets/styles/main.scss'
-import Login from './pages/auth/Login.jsx'
-import Search from './pages/search/Search.jsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./assets/styles/main.scss"
+import Login from "./pages/auth/Login.jsx"
+import Search from "./pages/search/Search.jsx"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-createRoot(document.getElementById('root')).render(
+//탠스택 쿼리 사용위해 필요
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <Login /> */}
-    <Search />
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      {/* <Login /> */}
+      <Search />
+    </QueryClientProvider>
+  </StrictMode>
 )
