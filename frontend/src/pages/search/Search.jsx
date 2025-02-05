b2ad9a2e3540ec6bdeadb5d61b3274d4682c0e35
hyejoony <jeonbok.joha@gmail.com>
2025-02-05 13:33:47 +0900

    Feat : 검색 페이지 ver3(최종완성)
    직전 커밋(오답노트:틀린 코드와 트러블 기록)에 기록을 다 지운 최종본 코드
    
    JIRA : #S12P11B111-55

--- frontend/src/pages/search/Search.jsx
+++ frontend/src/pages/search/Search.jsx
@@ -23,22 +23,9 @@ function Search() {
 
 
   // 화장품명 검색 스토어
-  const { setQuery, setFilterByUserPreference, setCategory, filterByUserPreference, category } = useSearchStore()
-  const { data: cosmetics, isLoading, isError, error, refetch } = useSearchCosmetics()
+  const { setQuery, setFilterByUserPreference, setCategory } = useSearchStore()
+  const { data: cosmetics, isLoading, isError, error } = useSearchCosmetics()
 
-
-  // 필요없어짐 form 안에 다 넣어놔서 누르는 순간 submit 됨
-  // 그리고 각각의 button-submit이 아니라 하나의 form 안에 모든 버튼 넣어놔서 아래 필요없음음
-  // // 검색바 검색 완료 여부
-  // const [isSearchSubmitted, setIsSearchSubmitted] = useState(false)
-
-  // // 검색 액션
-  // const handleSearchSubmit = (e) => {
-  //   e.preventDefault() // 새로고침 방지
-  //   console.log(`검색을 시도했습니다 :${searchWord}`)
-  //   setQuery(searchWord) // 사용자 검색어
-  //   setIsSearchSubmitted(true)
-  // }
 
   // 검색 액션 
   const submitEvent = (e) => {
@@ -50,25 +37,8 @@ function Search() {
 
   //#region 잘맞는 화장품 보기 버튼
 
-  
   // 잘 맞는 화장품 선택 여부 확인
   const [isActive, setIsActive] = useState(false);
-
-
-
-  // // '잘맞는 화장품 보기 필터링' 버튼
-  // const handleFilterClick = () => {
-  //   setFilterByUserPreference((prevState) => !prevState) // 토글기능
-
-  //   if (searchWord && !isSearchSubmitted) {
-  //     setQuery(searchWord)
-  //     setIsSearchSubmitted(true)
-  //   } else if (searchWord) {
-  //     setQuery(searchWord)
-  //   } // 검색 완료 버튼 누른 후 잘맞는화장품보기 필터링 버튼을 누르면 반환환
-
-  //   refetch()
-  // }
 
     // 잘 맞는 화장품 보기 버튼 클릭 감지
     const handleUserPreferBtnClick = () => {
@@ -81,46 +51,23 @@ function Search() {
   // 카테고리 박스 클릭 여부 확인
   const [isOpen, setIsOpen] = useState(false);
 
-  // 선택한 카테고리 value
-  // const [selectedOption, setSelectedOption] = useState(null);
-  const [selectedOption, setSelectedOption] = useState({
-    id : '' ,
-    name : ''
-  });
+  // 선택한 카테고리 value(UI)
+  const [selectedOption, setSelectedOption] = useState('');
 
 
   // 카테고리 옵션 드롭다운(UI)
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
-    
-  // 카테고리 선택 스토어(axios 요청)에 전달
-  // const handleCategorySelect = (selectedCategory) => {
-    // 전체선택 고려할 필요가 없었다.default은 null 값으로 되어있어서
-    // null은 null대로 전달되게 하면 됐다다
-    // setCategory(selectedCategory.id === "" ? null : selectedCategory.id)
 
-    // //검색 단어
-    // if (searchWord && !isSearchSubmitted) {
-    //   setQuery(searchWord)
-    //   setIsSearchSubmitted(true)
-
-    // 재선택시 반영되게 할 필요가 없었다 그냥 value가 전달되게 하면 됐다 
-    // } else if (selectedCategory.id !== category.id) {
-    //   setCategory(selectedCategory.id)
-    // }
-    // refetch()
-  }
   
   // 카테고리 클릭된 옵션 감지
-  //select box 커스텀 불가 ->  button 은 value값 전달이 안됨(id, name값 다루기기)
   const handleOptionClick = (option) => {
-    setSelectedOption({
-      id : option.id,
-      name : option.name,
-    });
-    setIsOpen(false); // 창닫기기
+    setSelectedOption(option.name);
+    // console.log(selectedOption)
+    setIsOpen(false) // 창닫기
     setCategory(option.id) 
+    console.log(option);
   };
   //#endregion
 
@@ -130,29 +77,10 @@ function Search() {
     if (isError) console.log("에러", error?.message)
   }, [isLoading, isError, error])
 
-  // // 실시간 검색어-연관검색어 목록
-  // // useMemo : computed
-  // const filteredCosmetics = useMemo(() => {
-  //   // if (!cosmetics) return [];
-  //   return cosmetics.filter(item =>
-  //     item.cosmeticName.includes(searchWord) //제품명만 반환
-  //   );
-  // }, [cosmetics, searchWord]);
-
   return (
     <div>
       <form onSubmit={submitEvent}>
         <SearchBar searchWord={searchWord} setSearchWord={setSearchWord} />
-
-      {/* 임시*/}
-      {/* <select value={category} onChange={(e) => handleCategorySelect(e.target.value)}>
-        <option value=""> 카테고리 선택</option>
-        {CATEGORIES.map((category) => (
-          <option key={category.id} value={category.id}>
-            {category.name}
-          </option>
-        ))}
-      </select> */}
 
       {/* 카테고리, 잘 맞는 화장품 검색 */}
       <div className="filter">
@@ -167,10 +95,8 @@ function Search() {
       {isOpen && (
         <ul className="category-options">
           {categoryOptions.map((option) => (
-            <li key={option.key}>
-            {/* useState -> object 형태로 id, 둘다 저장
-            button : value값이 없음 -> value useState 선언 필요요 */}
-              <button onClick={() => handleOptionClick(option)}>
+            <li key={option.id}>
+              <button  onClick={() => handleOptionClick(option)}>
                 {option.name}
               </button>
             </li>
@@ -186,16 +112,6 @@ function Search() {
       </button>
     </div>      
   </form>
-      
-      {/* <button
-        onClick={handleFilterClick}
-        className="filter-btn"
-        style={{
-          color: filterByUserPreference ? "#FDCFB7" : "#565656",
-        }}
-      >
-        잘맞는 화장품 보기
-      </button> */}
 
       {/* 화장품 검색결과/검색결과 없을 때의 화면 */}
     <SearchResult result={cosmetics} />
