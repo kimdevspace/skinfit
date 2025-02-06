152c89fc0fbebefba0f5fe85c3f13b927bc6a459
Daekyue <howeorb@naver.com>
2025-02-06 15:45:43 +0900

    Fix : deploy.sh 파일 재 수정정

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -7,12 +7,14 @@ docker pull $IMAGE_NAME
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
-# 볼륨 마운트 추가하여 빌드된 파일을 EC2의 Nginx와 공유
+# 컨테이너 실행
 docker run -d \
   --name $CONTAINER_NAME \
   --restart=always \
-  -v /var/www/html:/usr/share/nginx/html \
   $IMAGE_NAME
+
+# 컨테이너에서 빌드 파일들을 EC2의 Nginx 디렉토리로 복사
+docker cp $CONTAINER_NAME:/usr/share/nginx/html/. /var/www/html/
 
 echo "미사용 이미지 정리"
 docker image prune -f
