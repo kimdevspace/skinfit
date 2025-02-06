f7dbb98730e705ca8bbc5af8164dd928d94297b9
hyejoony <jeonbok.joha@gmail.com>
2025-02-06 17:56:48 +0900

    Chore : 마이페이지 route 설정
    마이페이지 route 설정 - :id 파라미터값 임의 제거거

--- frontend/src/routes/MyPageRoute.jsx
+++ frontend/src/routes/MyPageRoute.jsx
@@ -1,14 +1,16 @@
 /*MyPageRoute.jsx*/
 import { Routes, Route } from "react-router-dom";
+import MyPage from '../pages/mypage/MyPage.jsx';
 
 function MyPageRoute() {
-//   return (
-//     <Routes>
-//       <Route path=":id" element={<MyPage />} />
-//       <Route path=":id/edit" element={<EditUserInfo />} />
-//       <Route path=":id/ingredient" element={<IngredientDetail />} />
-//     </Routes>
-//   );
+  return (
+    <Routes>
+        <Route path="/" element={<MyPage />} />
+      {/* <Route path=":id" element={<MyPage />} /> */}
+      {/* <Route path=":id/edit" element={<EditUserInfo />} />
+      <Route path=":id/ingredient" element={<IngredientDetail />} /> */}
+    </Routes>
+  );
 }
 
 export default MyPageRoute;
