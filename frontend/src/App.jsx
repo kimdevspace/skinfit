3bb868b424206614d2f8950339705181a3ad63cc
hyejoony <jeonbok.joha@gmail.com>
2025-02-06 17:56:12 +0900

    Chore : route 설정
    마이페이지 구현을 위해 route 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import AuthRoutes from "./routes/AuthRoutes";
-// import MyPageRoute from "./routes/MyPageRoute";
+import MyPageRoute from "./routes/MyPageRoute";
 import HomeRoute from "./routes/HomeRoute";
 // import CosmeticsRoute from "./routes/CosmeticsRoute";
 // import Search from "../pages/search/Search";
@@ -20,7 +20,7 @@ function App() {
       <Route path="/auth/*" element={<AuthRoutes />} />
       
       {/* 마이페이지 라우트 */}
-      {/*<Route path="/mypage/*" element={<MyPageRoute />} />*/}
+      <Route path="/mypage/*" element={<MyPageRoute />} />
       
       {/* 제품 관련 페이지 라우트(제품 상세 페이지) */}
       {/* <Route path="/cosmetics/*" element={<CosmeticsRoute />} /> */}
