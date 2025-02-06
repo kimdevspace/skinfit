555ec0647480756181297270a8fcb4fa597d7eaf
Daekyue <howeorb@naver.com>
2025-02-06 15:32:39 +0900

    Fix : depoy.sh 파일 코드 수정정

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -7,10 +7,11 @@ docker pull $IMAGE_NAME
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
+# 볼륨 마운트 추가하여 빌드된 파일을 EC2의 Nginx와 공유
 docker run -d \
   --name $CONTAINER_NAME \
   --restart=always \
-  -p 3000:80 \
+  -v /var/www/html:/usr/share/nginx/html \
   $IMAGE_NAME
 
 echo "미사용 이미지 정리"
