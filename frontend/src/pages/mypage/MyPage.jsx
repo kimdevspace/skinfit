e741dba8b06e9fd0bcaf49769d83b007a83c9b3a
hyejoony <jeonbok.joha@gmail.com>
2025-02-07 15:50:32 +0900

    Feat : 마이페이지 밑그림(scss) 업데이트
    영역 완성(회원정보 영역, 나와 맞지않는 성분 TOP3 영역, 내가 등록한 화장품 영역, 리뷰 목록 영역)

--- frontend/src/pages/mypage/MyPage.jsx
+++ frontend/src/pages/mypage/MyPage.jsx
@@ -1,10 +1,12 @@
-import "./MyPage.scss"
+import "./MyPage.scss";
+import ToggleButton from "../../components/common/ToggleButton";
 
 function MyPage() {
   return (
-    <>
+    <div className="wrapper">
       <div className="edit-user-info">
-        <div className="user-name">먀먀님</div>
+        <span className="user-name">먀먀</span>
+        <span className="user-name2">님</span>
         <div className="user-skin-type">
           <span>#건성 </span>
           <span>#민감성 </span>
@@ -17,13 +19,16 @@ function MyPage() {
       <div className="top3-box">
         <div className="title">
           <h2>나와 맞지 않는 성분 TOP 3</h2>
-          <p className="question-text">내가 등록한 화장품에서 얼마나 발견됐을까?</p>
+          <p className="question-text">
+            내가 등록한 화장품에서 얼마나 발견됐을까?
+          </p>
         </div>
         <hr />
         <div className="ranking-text">
           <span className="rank-num">1</span>
           <span className="ingredient-name">아밀신남알</span>
-          <span className="count">9/10개</span>
+          <span className="count">9</span>
+          <span className="count2">/10개</span>
         </div>
         <div className="ingredient-detail-btn">
           <button className="detail-btn">성분 자세히 보기</button>
@@ -32,13 +37,25 @@ function MyPage() {
 
       <div className="cosmetic-wrapper">
         <h2>내가 등록한 화장품</h2>
-        <div className="toggle-container">
-          <button className="toggle-btn active">내가 좋아요한 리뷰</button>
-          <button className="toggle-btn">내가 작성한 리뷰</button>
+        <div className="set-position">
+          <ToggleButton btn1="맞는 화장품" btn2="맞지 않는 화장품" />
+          <button className="edit-del-btn">수정</button>
         </div>
+        <div className="cosmetic-list">
+          <span className="cosmetic-name">달바 미스트 에어퍼퓸 100ml</span>
+          <img src="" alt="" />
+        </div>
+        <hr />
       </div>
-    </>
-  )
+
+      <div className="review-wrapper">
+        <h2>리뷰 목록</h2>
+        <ToggleButton btn1="내가 좋아요한 리뷰" btn2="내가 작성한 리뷰" />
+      </div>
+      <hr className="hr-line" />
+      <p>회원탈퇴</p>
+    </div>
+  );
 }
 
-export default MyPage
+export default MyPage;
