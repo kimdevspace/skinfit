fd5c1943dbeda37ae77bbce448b1611320d5bf24
hyejoony <jeonbok.joha@gmail.com>
2025-02-10 17:56:49 +0900

    Feat : 리뷰 신고 기능 로직 연결 완료
    컴포넌트 삽입, 리뷰 신고 기능 로직 연결
    
    JIRA : S12P11B111-123

--- frontend/src/components/review/ReviewComplaintPopup.jsx
+++ frontend/src/components/review/ReviewComplaintPopup.jsx
@@ -1,20 +1,64 @@
-import './ReviewComplaintPopup.scss'
-
-import React from "react";
 import "./ReviewComplaintPopup.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useState, useRef } from "react";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
-function ReviewComplaintPopup({ closePopup }) {
+function ReviewComplaintPopup({ onClose, cosmeticId, reviewId }) {
+  //신고 데이터
+  const [reportData, setReportData] = useState('');
+
+  // 신고 post 요청
+  const reportReview = async (report) => {
+    const formData = new FormData();
+    formData.append("reason", report.reason);
+
+    return axios.post(
+      `/api/v1/cosmetics/${cosmeticId}/reviews/${reviewId}/report`,
+      formData
+    );
+  };
+
+  //react query
+  const mutation = useMutation({
+    mutationFn: reportReview,
+    onSuccess: () => {
+      console.log("신고접수 완료");
+    },
+    onError: (error) => {
+      console.error("신고 접수 에러", error);
+    },
+  });
+
+ //내용 작성 여부 
+  const contentErrorRef = useRef(null);
+
+  //신고 접수
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // 이유 작성 여부 확인
+    if (!reportData.trim()) {
+      contentErrorRef.current.classList.add('error');
+      return;
+    } else {
+      contentErrorRef.current.classList.remove('error');
+    }
+    
+    //mutate 메서드 (비동기처리)
+    mutation.mutate({ reason: reportData});
+  }
+
   return (
     <div className="overlay">
       <div className="review-complaint-popup">
-        <button className="close-btn" onClick={closePopup}>
+        <button className="close-btn" onClick={onClose}>
           <FontAwesomeIcon icon={faXmark} />
         </button>
 
         {/* 리뷰 신고 폼 */}
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="reason" className="popup-title">
             리뷰 신고하기
           </label>
@@ -22,9 +66,14 @@ function ReviewComplaintPopup({ closePopup }) {
             name="reason"
             id="reason"
             placeholder="신고 이유를 작성해주세요"
+            value={reportData}
+            // input 태그의 포커스를 벗어났을때 (즉, 입력이 끝났을때) 발생하는 이벤트
+            onChange={(e) => setReportData(e.target.value)}
           ></textarea>
-          <p className="error-msg">신고 이유가 작성되지 않았습니다</p>
-          <button className="submit-btn">신고하기</button>
+          <p className="error-msg" ref={contentErrorRef}>신고 이유가 작성되지 않았습니다</p>
+          <button className="submit-btn">
+            신고하기
+          </button>
         </form>
       </div>
     </div>
