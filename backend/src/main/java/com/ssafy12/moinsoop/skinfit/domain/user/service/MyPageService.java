ca263362205c21e94f4e467ef0ede98e9d63112a
김의중 <ejk5148@gmail.com>
2025-02-10 10:01:16 +0900

    Feat: 사용자가 등록한 화장품 조회 서비스단 비즈니스로직 구현
    리스트형태로 응답하기 위해 사용자경험 테이블을 가져와 convert 메서드를 통해 잘 맞는것, 잘 맞지 않는것으로 구분
    
    JIRA: S12P11B111-118

--- backend/src/main/java/com/ssafy12/moinsoop/skinfit/domain/user/service/MyPageService.java
+++ backend/src/main/java/com/ssafy12/moinsoop/skinfit/domain/user/service/MyPageService.java
@@ -1,10 +1,13 @@
 package com.ssafy12.moinsoop.skinfit.domain.user.service;
 
+import com.ssafy12.moinsoop.skinfit.domain.experience.entity.CosmeticExperience;
+import com.ssafy12.moinsoop.skinfit.domain.experience.entity.repository.CosmeticExperienceRepository;
 import com.ssafy12.moinsoop.skinfit.domain.experience.entity.repository.IngredientExperienceRepository;
 import com.ssafy12.moinsoop.skinfit.domain.ingredient.entity.Ingredient;
 import com.ssafy12.moinsoop.skinfit.domain.skintype.entity.SkinType;
 import com.ssafy12.moinsoop.skinfit.domain.skintype.entity.UserSkinType;
 import com.ssafy12.moinsoop.skinfit.domain.skintype.entity.repository.UserSkinTypeRepository;
+import com.ssafy12.moinsoop.skinfit.domain.user.dto.response.MyCosmeticsResponse;
 import com.ssafy12.moinsoop.skinfit.domain.user.dto.response.Top3UnSuitIngredientsResponse;
 import com.ssafy12.moinsoop.skinfit.domain.user.dto.response.UnSuitIngredientDto;
 import com.ssafy12.moinsoop.skinfit.domain.user.dto.response.UserNicknameAndUserSkinTypeResponse;
@@ -27,6 +30,7 @@ public class MyPageService {
     private final UserRepository userRepository;
     private final UserSkinTypeRepository userSkinTypeRepository;
     private final IngredientExperienceRepository ingredientExperienceRepository;
+    private final CosmeticExperienceRepository cosmeticExperienceRepository;
 
     @Transactional(readOnly = true)
     public UserNicknameAndUserSkinTypeResponse getUserNicknameAndSkinTypes(Integer userId) {
@@ -60,5 +64,39 @@ public class MyPageService {
                 .toList();
 
         return new Top3UnSuitIngredientsResponse(ingredients);
+    }
+
+    // 내가 등록한 화장품, 맞는 것과 안맞는 것으로 구분하여 응답
+    @Transactional(readOnly = true)
+    public MyCosmeticsResponse getAllMyCosmetics(Integer userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다"));
+
+        List<CosmeticExperience> experiences = cosmeticExperienceRepository.findByUserId(userId);
+
+        List<MyCosmeticsResponse.CosmeticExperienceDto> suitableList = experiences.stream()
+                .filter(CosmeticExperience::isSuitable)
+                .map(this::convertToDto)
+                .toList();
+
+        List<MyCosmeticsResponse.CosmeticExperienceDto> unsuitalbeList = experiences.stream()
+                .filter(exp -> !exp.isSuitable())
+                .map(this::convertToDto)
+                .toList();
+
+        return MyCosmeticsResponse.builder()
+                .suitableCosmetics(suitableList)
+                .unsuitableCosmetics(unsuitalbeList)
+                .build();
+    }
+
+    private MyCosmeticsResponse.CosmeticExperienceDto convertToDto(CosmeticExperience experience) {
+        return MyCosmeticsResponse.CosmeticExperienceDto.builder()
+                .cosmeticId(experience.getCosmetic().getCosmeticId())
+                .cosmeticBrand(experience.getCosmetic().getCosmeticBrand())
+                .cosmeticName(experience.getCosmetic().getCosmeticName())
+                .cosmeticVolume(experience.getCosmetic().getCosmeticVolume())
+                .imageUrl(experience.getCosmetic().getImageUrl())
+                .build();
     }
 }
