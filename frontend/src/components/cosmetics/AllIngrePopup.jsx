5f2827ec858996c47a62d1a575eabfbdfcc4c6f0
hanna <hanna0115@naver.com>
2025-02-11 14:53:06 +0900

    Feat: 전성분 보기 팝업 기능 구현 완료
    -성분 등급별 토글 기능 추가(사용자 가독성 위해)
    -성분 리스트 컴포넌트 생성(개발자 가독성 위해)
    
    JIRA: #S12P11B111-130

--- frontend/src/components/cosmetics/AllIngrePopup.jsx
+++ frontend/src/components/cosmetics/AllIngrePopup.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import "./AllIngrePopup.scss";
+import IngredientList from "./IngredientList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
@@ -10,22 +11,24 @@ const fetchAllIngredient = async (cosmeticId) => {
   const response = await axios.get(`/api/v1/${cosmeticId}/all-ingredients`, {
     headers: {
       // 'Authorization': `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    }
-  })
-  return response.data
-}
+      "Content-Type": "application/json",
+    },
+  });
+  return response.data;
+};
 
 function AllIngrePopup({ cosmeticId, closePopup }) {
   // 전성분 정보 가져오기
-  const { data: allIngredient, error, isLoading, isError } = useQuery({
-    queryKey: ['allIngredient', cosmeticId],
-    queryFn : () => fetchAllIngredient(cosmeticId),
-    enabled: !!cosmeticId // cosmeticId가 있을 때만 실행
-  })
-
-  // api에서 받아온 전성분 데이터
-  // const { ingredient } = allIngredient.data
+  const {
+    data: allIngredient,
+    error,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["allIngredient", cosmeticId],
+    queryFn: () => fetchAllIngredient(cosmeticId),
+    enabled: !!cosmeticId, // cosmeticId가 있을 때만 실행
+  });
 
   // 로딩, 에러 확인
   if (isLoading) {
@@ -34,8 +37,69 @@ function AllIngrePopup({ cosmeticId, closePopup }) {
   if (isError) {
     return console.log("에러", isLoading);
   }
-  
-  
+
+  // api에서 받아온 전성분 데이터
+  // const { low, moderate, high, others } = allIngredient.data
+
+  //#region 전성분 더미 데이터
+  const { low, moderate, high, others } = {
+    low: [
+      {
+        ingredientName: "히알루론산",
+        ewgScoreMin: 1,
+        ewgScoreMax: 2,
+      },
+    ],
+    moderate: [
+      {
+        ingredientName: "나이아신아마이드",
+        ewgScoreMin: 3,
+        ewgScoreMax: 6,
+      },
+    ],
+    high: [
+      {
+        ingredientName: "페녹시에탄올",
+        ewgScoreMin: 7,
+        ewgScoreMax: 9,
+      },
+    ],
+    others: [],
+  };
+  //#endregion
+
+
+  // 등급별 데이터 매핑
+  const grades = {
+    low: { title: "안전 등급", ingredients: low, className: "low" },
+    moderate: {
+      title: "보통 등급",
+      ingredients: moderate,
+      className: "moderate",
+    },
+    high: { title: "위험 등급", ingredients: high, className: "high" },
+    others: {
+      title: "미분류 등급",
+      ingredients: others,
+      className: "unclassified",
+    },
+  };
+
+  // 등급 클릭 시 토글
+  const [openGrades, setOpenGrades] = useState({
+    low: true, // 처음에는 안전 등급만 열려 있도록 설정
+    moderate: false,
+    high: false,
+    others: false,
+  });
+
+  const toggleGrade = (grade) => {
+    setOpenGrades((prev) => ({
+      ...prev,
+      [grade]: !prev[grade], // 클릭한 등급만 상태 변경
+    }));
+  };
+
   return (
     <div className="overlay">
       <div className="all-ingredient-popup">
@@ -44,67 +108,23 @@ function AllIngrePopup({ cosmeticId, closePopup }) {
         </button>
 
         <p className="popup-title">전성분 보기</p>
+
+        {/* 등급  */}
         <div className="all-ingredient">
-          <div className="grade-box low">
-            <p className="grade-title">안전 등급</p>
-            <div className="ingredient">
-              <p className="ingredient-name">정제수</p>
-              <p className="ewg-score">1</p>
+          {Object.keys(grades).map((key) => (
+            <div key={key} className={`grade-box ${grades[key].className}`}>
+              <p className="grade-title" onClick={() => toggleGrade(key)}>
+                {grades[key].title}
+              </p>
+              {/* 상태에 따라 성분 리스트 표시 */}
+              {openGrades[key] && (
+                <IngredientList
+                  className={grades[key].className}
+                  ingredients={grades[key].ingredients}
+                />
+              )}
             </div>
-            <div className="ingredient">
-              <p className="ingredient-name">히알루론산</p>
-              <p className="ewg-score">1-2</p>
-            </div>
-            <div className="ingredient">
-              <p className="ingredient-name">나이아신마이드</p>
-              <p className="ewg-score">2</p>
-            </div>
-          </div>
-          <div className="grade-box moderate">
-            <p className="grade-title">보통 등급</p>
-            <div className="ingredient">
-              <p className="ingredient-name">정제수</p>
-              <p className="ewg-score">1</p>
-            </div>
-            <div className="ingredient">
-              <p className="ingredient-name">히알루론산</p>
-              <p className="ewg-score">1-2</p>
-            </div>
-            <div className="ingredient">
-              <p className="ingredient-name">나이아신마이드</p>
-              <p className="ewg-score">2</p>
-            </div>
-          </div>
-          <div className="grade-box high">
-            <p className="grade-title">위험 등급</p>
-            <div className="ingredient">
-              <p className="ingredient-name">정제수</p>
-              <p className="ewg-score">1</p>
-            </div>
-            <div className="ingredient">
-              <p className="ingredient-name">히알루론산</p>
-              <p className="ewg-score">1-2</p>
-            </div>
-            <div className="ingredient">
-              <p className="ingredient-name">나이아신마이드</p>
-              <p className="ewg-score">2</p>
-            </div>
-          </div>
-          <div className="grade-box unclassified">
-            <p className="grade-title">미분류 등급</p>
-            <div className="ingredient">
-              <p className="ingredient-name">정제수</p>
-              <p className="ewg-score">1</p>
-            </div>
-            <div className="ingredient">
-              <p className="ingredient-name">히알루론산</p>
-              <p className="ewg-score">1-2</p>
-            </div>
-            <div className="ingredient">
-              <p className="ingredient-name">나이아신마이드</p>
-              <p className="ewg-score">2</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ frontend/src/components/cosmetics/IngredientList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './IngredientList.scss'
+
+function IngredientList({ className, ingredients }) {
+    return (
+      <div className="ingredient-list">
+        {ingredients.length > 0 ? (
+          ingredients.map((item, index) => (
+            <div key={index} className="ingredient">
+              <p className="ingredient-name">{item.ingredientName}</p>
+              <p className={`ewg-score ${className}`}>
+                {item.ewgScoreMin && `${item.ewgScoreMin}-`}{item.ewgScoreMax !== null ? `${item.ewgScoreMax}` : '-'}
+              </p>
+            </div>
+          ))
+        ) : (
+          <p className='not-included'>해당 성분 없음</p>
+        )}
+      </div>
+    );
+  }
+  
+
+export default IngredientList
