f840530cbe10c4f146ab70043cda45f3e4259f86
Hye-joon Jeon <jeonbok.joha@gmail.com>
2025-02-13 00:48:08 +0900

    Feat : ocr 기능 페이지 업데이트
    - scss 수정
    - 로직 연결(axios, mutation, usestate 등)
    - 사진 등록 컴포넌트 에러메시지 버그 해결 못함
    - 사진 컴포넌트 props 설정
    - 리뷰 등록 페이지 props 추가(사진 컴포넌트가 공통)
    
    JIRA : #S12P11B111-139

--- frontend/src/pages/ocr/OcrScanner.jsx
+++ frontend/src/pages/ocr/OcrScanner.jsx
@@ -1,136 +1,152 @@
-import "./OcrScanner.scss";
-import ImageUpload from "../../components/common/ImageUpload";
-import Button from "../../components/common/Button";
-import { useRef, useState } from "react";
-import axios from "axios";
-import { useMutation } from "@tanstack/react-query";
+import "./OcrScanner.scss"
+import ImageUpload from "../../components/common/ImageUpload"
+import Button from "../../components/common/Button"
+import { useRef, useState } from "react"
+import axios from "axios"
+import { useMutation } from "@tanstack/react-query"
 
 export default function OcrScanner() {
-
-  //카테고리 타입입
-  const allTypes = [
-    "로션",
-    "스킨",
-    "에센스",
-    "크림",
-    "클렌징",
-    "바디",
-    "선케어",
-  ];
-
   // ocr 사진 데이터
   const [ocrData, setOcrData] = useState({
     brandName: "",
     cosmeticName: "",
+    categories: "",
     images: [],
     capacity: "",
-  });
+  })
+
+  //카테고리 타입
+  const allTypes = ["로션", "스킨", "에센스", "크림", "클렌징", "바디", "선케어"]
+
+  // 카테고리 관리
+  const handleType = (type) => {
+    setOcrData((prevData) => ({
+      ...prevData,
+      categories: prevData.categories === type ? "" : type,
+    }))
+  }
 
   // 데이터 POST 요청
   const uploadOcr = async (ocr) => {
-    const formData = new FormData();
-    formData.append("brandName", ocr.brandName);
-    formData.append("cosmeticName", ocr.cosmeticName);
-    formData.append("capacity", ocr.capacity);
-    ocr.images.forEach((file) => formData.append("images", file));
-    console.log("ocr 사진 upload 한다");
+    const formData = new FormData()
+    formData.append("brandName", ocr.brandName)
+    formData.append("cosmeticName", ocr.cosmeticName)
+    formData.append("capacity", ocr.capacity)
+    formData.append("categories", ocr.categories)
+    ocr.images.forEach((file) => formData.append("images", file))
+    console.log("ocr 사진 upload 한다")
 
     return axios.post(`products/ocr`, formData, {
       headers: {
         // Authorization: `Bearer ${jwtToken}`,
         "Content-Type": "multipart/form-data",
       },
-    });
-  };
+    })
+  }
 
   const mutation = useMutation({
     mutationFn: uploadOcr,
     onSuccess: () => {
-      alert("사진이 등록되었어요.");
-      setOcrData({ rating: "", reviewContent: "", images: [] }); //?
+      alert("사진이 등록되었어요.")
+      setOcrData({ brandName: "",
+        cosmeticName: "",
+        categories: "",
+        images: [],
+        capacity: "",}) //?
     },
     onError: () => {
-      alert("사진 등록에 실패했어요.");
+      alert("사진 등록에 실패했어요.")
     },
-  });
+  })
 
-  const handleInputChange = (e) => {
-  
-  };
+  //미입력 에러메시지 관리리
+  const brandNameErrorRef = useRef(null)
+  const cosmeticNameErrorRef = useRef(null)
+  const capacityErrorRef = useRef(null)
+  const categoriesErrorRef = useRef(null)
+  // const imagesErrorRef = useRef(null)
 
-  const brandNameErrorRef = useRef(null);
-  const cosmeticNameErrorRef = useRef(null);
-  const capacityErrorRef = useRef(null);
+  // ocr 이미지 필수제출 관리
+  const [imageError, setImageError] = useState(false)
 
+  // 폼 제출출
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    if (!ocrData.brandName) {
-      brandNameErrorRef.current.classList.add("error");
-      return;
+    let hasError = false
+    const fields = [
+      { name: "brandName", ref: brandNameErrorRef },
+      { name: "cosmeticName", ref: cosmeticNameErrorRef },
+      { name: "capacity", ref: capacityErrorRef },
+      { name: "categories", ref: categoriesErrorRef },
+      // { name: "images", ref: imagesErrorRef },
+    ]
+
+    fields.forEach((field) => {
+      const value = ocrData[field.name]
+      if (!value) {
+        field.ref.current.classList.add("error")
+        hasError = true
+      } else {
+        field.ref.current.classList.remove("error")
+      }
+    })
+
+    if (ocrData.images.length === 0) {
+      setImageError(true)
+      hasError = true
     } else {
-      brandNameErrorRef.current.classList.remove("error");
+      setImageError(false)
     }
 
-    if (!ocrData.cosmeticName) {
-      cosmeticNameErrorRef.current.classList.add("error");
-      return;
-    } else {
-      cosmeticNameErrorRef.current.classList.remove("error");
+    if (!hasError) {
+      mutation.mutate(ocrData)
     }
-
-    if (!ocrData.capacity) {
-      capacityErrorRef.current.classList.add("error");
-      return;
-    } else {
-      capacityErrorRef.current.classList.remove("error");
-    }
-
-    mutation.mutate(ocrData);
-  };
+  }
 
   return (
     <div className="ocr-register-form">
       <div className="input-wrapper">
         <p className="input-title">상품명</p>
         <input id="name" type="text" placeholder="상품명을 입력해주세요" />
+        <p className="error-msg" ref={cosmeticNameErrorRef}>
+          상품명 입력은 필수예요
+        </p>
       </div>
       <div className="input-wrapper">
         <p className="input-title">브랜드명</p>
         <input id="name" type="text" placeholder="브랜드명을 입력해주세요" />
+        <p className="error-msg" ref={brandNameErrorRef}>
+          브랜드명 입력은 필수예요
+        </p>
       </div>
 
       <div className="category-group">
         <label>카테고리</label>
         <div className="skintype-options">
           {allTypes.map((type) => (
-            <button
-              key={type}
-              type="button"
-              className={`skin-btn ${
-                allTypes.includes(type) ? "selected" : ""
-              }`}
-              onClick={() => handleType(type)}
-            >
+            <button key={type} type="button" className={`skin-btn ${ocrData.categories === type ? "selected" : ""}`} onClick={() => handleType(type)}>
               {type}
             </button>
           ))}
         </div>
+        <p className="error-msg" ref={categoriesErrorRef}>
+          카테고리 선택은 필수예요{" "}
+        </p>
       </div>
 
       <div className="input-wrapper">
         <p className="input-title">상품용량(mL)</p>
         <input id="name" type="text" placeholder="상품용량을 입력해주세요" />
+        <p className="error-msg" ref={capacityErrorRef}>
+          상품용량 입력은 필수예요
+        </p>
       </div>
 
       {/* 리뷰 사진 등록 */}
-      <ImageUpload
-        images={ocrData.images}
-        setImages={setOcrData}
-        maxImages={1}
-      />
+      <ImageUpload images={ocrData.images} setImages={setOcrData} maxImages={1} dataType="ocr" onError={setImageError} error={imageError} />
 
-      <Button text="등록하기" color="pink" type="submit" />
+      <Button text="등록하기" color="pink" type="submit" onSubmit={handleSubmit} />
     </div>
-  );
+  )
 }
