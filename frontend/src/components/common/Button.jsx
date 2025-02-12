e451febe3498a32bfb8fe796b9f5498cab0b79bc
hyejoony <jeonbok.joha@gmail.com>
2025-02-12 15:13:29 +0900

    Fix : 버튼 컴포넌트 props 추가가

--- frontend/src/components/common/Button.jsx
+++ frontend/src/components/common/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Button.scss";
 
-function Button({ text, color, type = "button", onClick }) {
+function Button({ text, color, type = "button", onSubmit }) {
   return (
-    <button className={`button ${color}`} type={type} onClick={onClick}>
+    <button className={`button ${color}`} type={type} onClick={onSubmit}>
       {text}
     </button>
   );
