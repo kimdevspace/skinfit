c38464436c6ff05aa048d63c3e1f0e1096094ccd
hanna <hanna0115@naver.com>
2025-02-12 17:57:30 +0900

    Feat: 검색 팝업창 기능 구현중
    나와 잘 맞는 / 안 맞는, 화장품 / 성분 검색하는 팝업창 스타일링 중
    
    JIRA: S12P11B111-138

---
+++ frontend/src/components/search/SearchPopup.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./SearchPopup.scss";
+import SearchBar from "./SearchBar";
+import SearchResult from "./SearchResult";
+import Button from '../common/Button'
+
+function SearchPopup() {
+  return (
+    <div className="search-popup-overlay">
+      <div className="search-popup">
+        <hr />
+        <SearchBar />
+        
+        {/* 검색 결과 박스 */}
+        <div className="search-result-box">
+          <SearchResult />
+        </div>
+
+        {/* 선택한 화장품 / 성분 확인 박스 */}
+        <div className="my-list-box">
+          <p className="title">맞지 않는 화장품</p>
+          <div className="my-search-list">
+            <p className="info-msg">나와 맞지 않는 화장품을 추가해주세요</p>
+            <div className="my-search-item">
+              <img src="" alt="" />
+              <div className="text-box">
+                <p className="brand-name">달바</p>
+                <p className="main-text">미스트 화이트 머스크 트러플 세럼럼</p>
+              </div>
+              <div className="btn-box">
+                <button className="btn add-btn">추가</button>
+                <button className="btn remove-btn">삭제</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Button text='등록하기' color='pink' />
+      </div>
+    </div>
+  );
+}
+
+export default SearchPopup;
