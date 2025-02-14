3069bde7c47cb708348c0b8ecd7071780870facc
rim.ssafy <rim.ssafy@gmail.com>
2025-02-14 10:36:18 +0900

    Fix: 중복 CosmeticRepository 삭제
    JIRA: #S12P11B111-145

--- backend/src/main/java/com/ssafy12/moinsoop/skinfit/domain/cosmetic/repository/CosmeticRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy12.moinsoop.skinfit.domain.cosmetic.repository;
-
-import com.ssafy12.moinsoop.skinfit.domain.cosmetic.entity.Cosmetic;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CosmeticRepository extends JpaRepository<Cosmetic, Integer> {
-    List<Cosmetic> findByCosmeticNameContainingIgnoreCase(String query);
-}
