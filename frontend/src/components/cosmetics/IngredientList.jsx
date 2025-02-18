3b759442739e04cc0ca5d2c549e5217e898d0b55
hanna <hanna0115@naver.com>
2025-02-18 13:38:34 +0900

    Fix: 전성분 보기 팝업 api 요청 주소 수정정

--- frontend/src/components/cosmetics/IngredientList.jsx
+++ frontend/src/components/cosmetics/IngredientList.jsx
@@ -5,8 +5,8 @@ function IngredientList({ className, ingredients }) {
     return (
       <div className="ingredient-list">
         {ingredients.length > 0 ? (
-          ingredients.map((item, index) => (
-            <div key={index} className="ingredient">
+          ingredients.map((item) => (
+            <div key={item.ingredientId} className="ingredient">
               <p className="ingredient-name">{item.ingredientName}</p>
               <p className={`ewg-score ${className}`}>
                 {item.ewgScoreMin && `${item.ewgScoreMin}-`}{item.ewgScoreMax !== null ? `${item.ewgScoreMax}` : '-'}
