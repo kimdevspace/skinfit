aba5fefde8a583a929d3ea6880153750285c30f0
김의중 <ejk5148@gmail.com>
2025-02-21 00:03:39 +0900

    Fix: 나와 관련된 리뷰 응답 dto 수정

--- backend/src/main/java/com/ssafy12/moinsoop/skinfit/domain/user/dto/response/MyReviewResponse.java
+++ backend/src/main/java/com/ssafy12/moinsoop/skinfit/domain/user/dto/response/MyReviewResponse.java
@@ -3,8 +3,10 @@ package com.ssafy12.moinsoop.skinfit.domain.user.dto.response;
 import com.ssafy12.moinsoop.skinfit.domain.cosmetic.entity.Cosmetic;
 import com.ssafy12.moinsoop.skinfit.domain.review.entity.Review;
 import com.ssafy12.moinsoop.skinfit.domain.review.entity.ReviewImage;
+import com.ssafy12.moinsoop.skinfit.domain.user.entity.User;
 import lombok.*;
 import java.time.LocalDateTime;
+import java.time.Year;
 import java.util.Comparator;
 import java.util.List;
 
@@ -26,6 +28,7 @@ public class MyReviewResponse {
         private CosmeticInfo cosmetic;
         private List<ImageDto> images;
         private int likes;
+        private UserInfo writer;
 
         public static ReviewDto from(Review review) {
             return ReviewDto.builder()
@@ -39,7 +42,30 @@ public class MyReviewResponse {
                             .sorted(Comparator.comparing(ImageDto::getSequence))
                             .toList())
                     .likes(review.getReviewLikes().size())
+                    .writer(new UserInfo(review.getUser()))
                     .build();
+        }
+    }
+
+    @Getter
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class UserInfo {
+        private Integer userId;
+        private String nickname;
+        private String ageRange;
+
+        private UserInfo(User user) {
+            this.userId = user.getUserId();
+            this.nickname = user.getNickname();
+            this.ageRange = calculateAgeRange(user.getBirthYear());
+        }
+
+        private String calculateAgeRange(Year birthYear) {
+            if (birthYear == null) return "알 수 없음";
+
+            int age = Year.now().getValue() - birthYear.getValue();
+            int ageGroup = (age / 10) * 10;
+            return String.format("%d대", ageGroup);
         }
     }
 
