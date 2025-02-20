4cbf0b8c44cf1fce2d47b882488aa6d92b9cb374
hyejoony <jeonbok.joha@gmail.com>
2025-02-20 23:37:00 +0900

    Fix: isRegistered 불리언 값으로 수정정

--- frontend/src/pages/auth/OAuthCallback.jsx
+++ frontend/src/pages/auth/OAuthCallback.jsx
@@ -1,34 +1,22 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import useAuthStore from '../../stores/Auth.js';
-import './OAuthCallback.scss';
+import useAuthStore from "../../stores/Auth.js";
+import "./OAuthCallback.scss";
 
 function OAuthCallback() {
-  const navigate = useNavigate()
   const setAuth = useAuthStore((state) => state.setAuth);
-  
+
   useEffect(() => {
     // URL 파라미터에서 데이터 추출
     const params = new URLSearchParams(window.location.search);
     const accessToken = params.get("accessToken");
-    const isRegistered = params.get("isRegistered");
-    console.log('OAuthCallback')
-    console.log('accessToken', accessToken)
+    const isRegisteredStr = params.get("isRegistered");
+
+    // 문자열을 불리언으로 변환
+    const isRegistered = isRegisteredStr === "true";
 
     if (accessToken) {
       // 토큰 저장
-      setAuth(
-        accessToken,
-        'USER',
-        isRegistered
-      );
-
-      // 회원정보 기록 상태에 따른 페이지 이동
-      if (isRegistered === "true") {
-        navigate('/');
-      } else {
-        navigate('/auth/userform');
-      }
+      setAuth(accessToken, "USER", isRegistered);
     }
   }, []);
 
