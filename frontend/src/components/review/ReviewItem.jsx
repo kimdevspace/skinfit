d41ef9f5737d88ab2a51f5211c075da28f72869e
hyejoony <jeonbok.joha@gmail.com>
2025-02-21 03:59:59 +0900

    Fix: 리뷰 컴포넌트 오류 수정
    화장품 상세조회 리뷰, 마이페이지 리뷰에서 참조하는 키값이 달라 한쪽에서 나오지 않는 문제 발생 -> 조건문으로 처리

--- frontend/src/components/review/ReviewItem.jsx
+++ frontend/src/components/review/ReviewItem.jsx
@@ -176,8 +176,8 @@ export default function ReviewItem({ review, reviewType }) {
 
       {reviewType !== "myReviews" ? (
         <div className="user-info-section">
-          <span className="user-name">{review.userNickname}님</span>
-          <div className="age-skintype">{review.userAgeGroup} | {review.userSkinType}
+          <span className="user-name">{reviewType === 'generalReviews' ? review.userNickname : review.nickname}님</span>
+          <div className="age-skintype">{reviewType === 'generalReviews' ? review.userAgeGroup : review.ageRange} | {review.userSkinType}
             
           </div>
           <span className={`user-answer ${scoreMap[review.score].class}`}>
