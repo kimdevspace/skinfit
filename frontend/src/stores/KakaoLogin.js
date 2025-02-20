b6c67b7f61c0558ad3162987b123e32fbb2f4c54
hyejoony <jeonbok.joha@gmail.com>
2025-02-20 21:50:34 +0900

    test: html에 있는 json 데이터 찾기

--- frontend/src/stores/KakaoLogin.js
+++ frontend/src/stores/KakaoLogin.js
@@ -3,6 +3,10 @@ export const useKakaoLogin = () => {
    // 간단하게 직접 카카오 로그인 페이지로 리다이렉트
    const initiateKakaoLogin = () => {
     window.location.href = '/api/v1/oauth/kakao/login';
+
+    const preElement = document.querySelector("pre");
+
+    console.log(preElement)
   };
   
     return { initiateKakaoLogin };
